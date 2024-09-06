79ccbc6783a43d4a4e619fff6b38f88533b30bb5
madara4000 <yagomartins4000@gmail.com>
2024-09-06 17:40:52 -0300

    att nova versão agenda definições

--- Agenda Final/src/Agenda.c
+++ Agenda Final/src/Agenda.c
@@ -6,7 +6,27 @@
 #define INT sizeof(int)
 #define TINT sizeof(int) * 3
 #define NOME sizeof(char) * 10
+#define TELEFONE sizeof(char) * 15
+#define PESSOA sizeof(NOME+INT+TELEFONE)
+void remover_pessoa(char* nome)
+{
 
+}
+void adicionar_pessoa( char* nome, int* idade, char* telefone)
+{
+
+}
+void heap()
+{
+
+}
+void listar()
+{
+    
+}
+void lista_duplamente_ligada(void *){
+
+}
 int main()
 {  void *pbuffer=(void*) malloc(sizeof())
     
